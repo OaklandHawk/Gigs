ec0d77a3647d8af2ced62b9c0b190d1dfe5a0d5b
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-04 15:31:35 -0700

    Added LoginViewController and set the class

---
+++ Gigs/View Controller/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Gigs
+//
+//  Created by Taylor Lyles on 9/4/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
