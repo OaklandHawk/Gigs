d1372291a5010b8137400eac167a23bed4e6d11f
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-05 07:44:16 -0700

    logIn function added

--- Gigs/Model Controller/GigController.swift
+++ Gigs/Model Controller/GigController.swift
@@ -62,4 +62,53 @@ class GigController {
 			completion(nil)
 		}.resume()
 	}
+	
+	func logIn(with user: User, completion: @escaping (Error?) -> ()) {
+		let logInUrl = baseURL
+			.appendingPathComponent("users")
+			.appendingPathComponent("login")
+		
+		var request = URLRequest(url: logInUrl)
+		request.httpMethod = HTTPMethod.post.rawValue
+		request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+		
+		do {
+			request.httpBody = try JSONEncoder().encode(user)
+		} catch {
+			NSLog("Error encoding user object: \(error)")
+			completion(error)
+			return
+		}
+		
+		URLSession.shared.dataTask(with: request) { (data, response, error) in
+			
+			if let response = response as? HTTPURLResponse,
+				response.statusCode != 200 {
+				completion(NSError())
+				return
+			}
+			
+			if let error = error {
+				NSLog("Error logging in: \(error)")
+				completion(error)
+				return
+			}
+			
+			guard let data = data else {
+				completion(NSError(domain: "No data recieved", code: -1, userInfo: nil))
+				return
+			}
+			
+			do {
+				let bearer = try JSONDecoder().decode(Bearer.self, from: data)
+				self.bearer = bearer
+				completion(nil)
+			} catch {
+				NSLog("Token not recieved: \(error)")
+				completion(error)
+				return
+			}
+		}.resume()
+	}
 }
+
